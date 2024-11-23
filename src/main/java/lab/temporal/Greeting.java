e94bfbb73d83cf4b1ffa523043b14d009f86b0fb
Maarten Derksen <maderkse@gmail.com>
2024-11-23 16:48:43 +0000

    H3 Solution

--- src/main/java/lab/temporal/Greeting.java
+++ src/main/java/lab/temporal/Greeting.java
@@ -1,5 +1,11 @@
 package lab.temporal;
 
+import io.temporal.workflow.WorkflowInterface;
+import io.temporal.workflow.WorkflowMethod;
+
+@WorkflowInterface
 public interface Greeting {
+
+   @WorkflowMethod
    String greetSomeone(String name);
 }
